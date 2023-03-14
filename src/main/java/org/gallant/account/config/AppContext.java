8eca22e4cec333aacae25fa3dd73f5033ca82cc4
gallant <gallantkong@163.com>
2023-03-14 23:32:45 +0800

    add jetty config

--- src/main/java/org/gallant/account/config/AppContext.java
+++ src/main/java/org/gallant/account/config/AppContext.java
@@ -5,6 +5,7 @@ import org.eclipse.jetty.util.thread.QueuedThreadPool;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
+import org.springframework.boot.web.server.Compression;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -37,11 +38,16 @@ public class AppContext {
     @Bean
     public ServletWebServerFactory servletContainer() {
         JettyServletWebServerFactory jetty = new JettyServletWebServerFactory();
-        QueuedThreadPool tp = new QueuedThreadPool(4,4, 60000, new BlockingArrayQueue<>(1));
+        QueuedThreadPool tp = new QueuedThreadPool(200,8, 60000, new BlockingArrayQueue<>(1));
         jetty.setThreadPool(tp);
         jetty.setPort(Integer.parseInt(serverPort.trim()));
         MyJettyServerCustomizer myJettyServerCustomizer = new MyJettyServerCustomizer();
         jetty.addServerCustomizers(myJettyServerCustomizer);
+        Compression compression = new Compression();
+        compression.setEnabled(true);
+        // 注意不生效，会被org.springframework.boot.autoconfigure.web.servlet.ServletWebServerFactoryCustomizer#customize重写
+        jetty.setCompression(compression);
+        jetty.setServerHeader("my-server-header-value");
         return jetty;
     }
 }
